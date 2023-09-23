3596ac8e3c4d2502de1ef5f6644a076a305ef781
BorisKGB <flot3.sky@yandex.ru>
2023-09-23 12:40:33 +0300

    add Task3 solution

---
+++ src/main/java/ru/study/seminar2/Task3.java
@@ -0,0 +1,53 @@
+package ru.study.seminar2;
+
+/*
+Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+public static void main(String[] args) throws Exception {
+   try {
+       int a = 90;
+       int b = 3;
+       System.out.println(a / b);
+       printSum(23, 234);
+       int[] abc = { 1, 2 };
+       abc[3] = 9;
+   } catch (Throwable ex) {
+       System.out.println("Что-то пошло не так...");
+   } catch (NullPointerException ex) {
+       System.out.println("Указатель не может указывать на null!");
+   } catch (IndexOutOfBoundsException ex) {
+       System.out.println("Массив выходит за пределы своего размера!");
+   }
+}
+public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+   System.out.println(a + b);
+}
+
+ */
+
+public class Task3 {
+    // delete throws exception, already catch all(?) possible
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        // most generic exception must be the last one in catch list
+        // (probably should also change Throwable to Exception)
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    // got objects, can get null`s -> use NullPointerException instead FileNotFoundException
+    public static void printSum(Integer a, Integer b) throws NullPointerException {
+        System.out.println(a + b);
+    }
+}
